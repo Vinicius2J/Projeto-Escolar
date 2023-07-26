2b04fcb2d21bde8d32390536c49841e33be9a230
Jose Vinicius <102199272+Vinicius2J@users.noreply.github.com>
2023-07-25 22:53:22 -0300

    Add files via upload

---
+++ scripts/sistema-login/sistemaLogin.js
@@ -0,0 +1,4 @@
+let inputUserLogin = document.getElementById("userLogin");
+let inputPassowordLogin = document.getElementById("userPassword");
+const buttonEnterLogin = document.getElementById("btnEntrarLogin");
+const spanNoAccount = document.getElementById("noAccount");
---
+++ scripts/sistema-register/sistema-register.js
@@ -0,0 +1,9 @@
+sistemaLogin.js;
+
+document.addEventListener("DOMContentLoaded", function() {
+    const spanNoAccount = document.getElementById("noAccount");
+
+    spanNoAccount.addEventListener("click", () => {
+        
+    })
+})
